5dbde6c31a8dca2cfe2cc5f29806c46b52bc2cc0
Alissa Huskey <alissa.huskey@gmail.com>
2020-06-09 18:48:29 -0600

    Add computer-games/

---
+++ computer-games/chapter_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# ##########################################################
+#   Invent Your Own Computer Games with Python            ##
+#   Chapther 2:  Writing Programs                         ##
+#
+#
+# http://inventwithpython.com/invent4thed/chapter2.html
+# ##########################################################
+
+
+# Recommended repl.it settings
+# -----------------------------
+# indent type: spaces
+# indent size: 4 spaces
+# wrapping: none
+# code intelligence: off
