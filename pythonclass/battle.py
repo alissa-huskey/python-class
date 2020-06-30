382421ea101d1acf2d4a0ab18f0e01eb58642d20
Alissa Huskey <alissa.huskey@gmail.com>
2020-06-30 17:46:23 -0600

    Finish battle.py script
    - Move files from `pythonclass/misky/` to `pythonclass/`
    - Move pet data to `pets` module

--- pythonclass/battle.py
+++ pythonclass/battle.py
@@ -8,55 +8,11 @@
 import random
 import time
 import messenger
+from pets import PICS, PETS
 
 # ## Global Variables ########################################################
 
 MAX_HEALTH = 100
-
-# a hash of pet pics
-# species: pic
-PICS = {
-    'cat': "=^..^=",
-    'fish': "<`)))><",
-    'owl': "{O,o}",
-    'snake': "_/\\__/\\_/--{ :>~",
-    'bat': "/|\\^..^/|\\",
-    'monkey': "@('_')@",
-    'pig': "^(*(oo)*)^",
-    'mouse': "<:3 )~~~",
-    'bird': ",(u°)>",
-    'cthulhu': "^(;,;)^",
-    'fox': "-^^,--,~",
-}
-
-# nested list of pets
-#   each element of the list is dictionary
-#
-#   Below is is the same as:
-#
-#   FLUFO = {...}
-#   SCALEY = {...}
-#   PETS = [ FLUFO, SCALEY ]
-#
-#   You can access an element the way you normally would, so we could get the
-#     details for a pet with the syntax `variable[index]`:
-#
-#     pet = PETS[3]
-#     print(pet['name'], "says hello!")
-#
-#   You can access also access nested elements from a list by adding the `[]`
-#     reference straight to the end of the previous `[]` with the syntax
-#     `variable[index][key]`.
-#
-#     print(PETS[3]['name'], "says hello!")
-#
-
-PETS = [
-    {'name': "Flufosourus", 'species': "cat"},
-    {'name': "Scaley", 'species': "fish"},
-    {'name': "Count Chocula", 'species': "bat"},
-    {'name': "George", 'species': "monkey"},
-]
 
 # the range of damage each player can do
 #
