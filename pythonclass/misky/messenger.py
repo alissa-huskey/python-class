fa696739f7c52691b5abc88f7217efa3ade89f93
Alissa Huskey <alissa.huskey@gmail.com>
2020-06-30 17:32:47 -0600

    Clean up and comment py_battle.py and messenger.py
    - refactor py_battle.py to break up main into logical functions
    - add detailed comments

--- pythonclass/misky/messenger.py
+++ pythonclass/misky/messenger.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""A module for relaying messages to and from the player."""
+"""A module for relaying messages to and from the user"""
 
 import sys
 
 # the max width of the screen
-WIDTH = 48
+WIDTH = 55
 
 # what command always means quit
 QUIT_COMMAND = "q"
+
+
+def exit_if_quit(response):
+    """Exit the game if response is the quit command"""
+    if response == QUIT_COMMAND:
+        sys.exit()
+
+
+def header(text):
+    print("\n"+text)
+    line("=")
 
 
 def line(char="-"):
@@ -17,17 +28,34 @@ def line(char="-"):
     print(char * WIDTH)
 
 
-def center(text):
+def cols(left_text, right_text, rcol_width):
+    """Print text in two columns, the right column right-aligned"""
+    lcol_width = WIDTH - rcol_width - 1
+    left = left_text.ljust(lcol_width)
+    right = right_text.rjust(rcol_width)
+    print(left, right)
+
+
+def center(text, char=" ", padding=1):
     """Print text centered on the screen"""
-    print(text.center(WIDTH), "\n")
+    print(text.center(WIDTH, char), padding*"\n")
 
 
-def info(text):
-    """Print text to the user formatted as info"""
-    print("  ", text)
+def info(*args):
+    """Print text to the user formatted as info (indented)"""
+    args = map(str, args)
+    print("  ", " ".join(args))
 
 
-def exit_if_quit(response):
-    """Exit the game if response is the quit command"""
-    if response == QUIT_COMMAND:
-        sys.exit()
+def ask(prompt=""):
+    """Ask the user for input using padded prompt. Exit for quit command or
+       return response."""
+
+    # Add a space to the end of the prompt if one is passed and it does not
+    # already have one
+    if len(prompt) > 0 and not prompt.endswith(" "):
+        prompt += " "
+
+    response = input(prompt)
+    exit_if_quit(response)
+    return response
--- pythonclass/misky/py_battle.py
+++ pythonclass/misky/py_battle.py
@@ -3,12 +3,18 @@
 
 """PyPet Battle Game"""
 
+# ### Imports ################################################################
+
 import random
 import time
 import messenger
 
+# ## Global Variables ########################################################
+
 MAX_HEALTH = 100
 
+# a hash of pet pics
+# species: pic
 PICS = {
     'cat': "=^..^=",
     'fish': "<`)))><",
@@ -23,131 +29,220 @@ PICS = {
     'fox': "-^^,--,~",
 }
 
+# nested list of pets
+#   each element of the list is dictionary
+#
+#   Below is is the same as:
+#
+#   FLUFO = {...}
+#   SCALEY = {...}
+#   PETS = [ FLUFO, SCALEY ]
+#
+#   You can access an element the way you normally would, so we could get the
+#     details for a pet with the syntax `variable[index]`:
+#
+#     pet = PETS[3]
+#     print(pet['name'], "says hello!")
+#
+#   You can access also access nested elements from a list by adding the `[]`
+#     reference straight to the end of the previous `[]` with the syntax
+#     `variable[index][key]`.
+#
+#     print(PETS[3]['name'], "says hello!")
+#
 
-# our list of pets
-# this is a list, where each element of the list is a dictionary
 PETS = [
-    {
-        'name': "Flufosourus",
-        'species': "cat",
-    },
-    {
-        'name': "Scaley",
-        'species': "fish",
-    },
-    {
-        'name': "Count Chocula",
-        'species': "bat",
-    },
-    {
-        'name': "George",
-        'species': "monkey",
-    },
+    {'name': "Flufosourus", 'species': "cat"},
+    {'name': "Scaley", 'species': "fish"},
+    {'name': "Count Chocula", 'species': "bat"},
+    {'name': "George", 'species': "monkey"},
 ]
 
 # the range of damage each player can do
-# this is a set -- just like a list, except it is immutable
+#
+#   this is a data type called a tuple
+#   it is just like a list, except it is
+#   immutable, meaning it cannot be changed
+
 POWER = (10, 30)
 
 # the number of seconds to pause for dramatic effect
 DELAY = 1
 
+# a list attacks
 FIGHTIN_WORDS = [
     "nips at",
     "takes a swipe at",
     "glares sternly at",
-    "ferociously boofs",
+    "ferociously smacks",
+    "savagely boofs",
     "is awfully mean to",
     "can't even believe",
     "throws mad shade at",
 ]
 
 
-def prep():
-    """Set the health and pic of all pets"""
-    for pet in PETS:
-        pet["health"] = MAX_HEALTH
-        pet["pic"] = PICS[pet["species"]]
+# ## Functions ###############################################################
 
+# ### pet functions ###
+#
 
-def lotto():
-    """Return two randomly chosen pets"""
-    random.shuffle(PETS)
-
-    # This is called a slice--we are removing the first two elements of the
-    # list
-    return PETS[0:2]
-
-
-def attack(foe):
-    """A random amount of damage is inflicted on foe"""
-    damage = random.randint(POWER[0], POWER[1])
-    foe["health"] -= damage
-    return damage
+def init(pets):
+    """Set attributes for each of list of pets"""
+    for pet in pets:
+        pet['health'] = MAX_HEALTH
+        pet['pic'] = PICS[pet['species']]
+        pet['title'] = f"{pet['name']} the {pet['species'].capitalize()}"
 
 
 def show(pet):
     """Print the details about a pet"""
-    print("  ", pet["name"], pet["pic"], pet["health"], "of", MAX_HEALTH)
+    messenger.cols(
+        f"{pet['name']} {pet['pic']}",
+        f"{pet['health']} of {MAX_HEALTH}",
+        rcol_width=len("100 of 100")
+    )
 
 
-def main():
-    """PyPet Fight Game"""
-    print("\nWelcome to the THUNDERDOME!")
-    messenger.line("=")
+# ### game event functions ###
+#
 
-    prep()
-    fighters = lotto()
-    winner = None
-    current = random.randint(0, 1)
+def lotto():
+    """Return two randomly chosen PETS"""
+    # randomly reorder the PETS list
+    random.shuffle(PETS)
 
+    # This is called a slice
+    # it gets items 0 to (but not including) 2
+    return PETS[0:2]
+
+
+def attack(foe):
+    """Inflict a random amount of damage is inflicted on foe, then return the
+       damage and attack used"""
+    # choose an attack
+    act = random.choice(FIGHTIN_WORDS)
+
+    # randomly set damage
+    damage = random.randint(POWER[0], POWER[1])
+
+    # the -= operator is the same as:
+    # foe['health'] = foe['health'] - damage
+    foe['health'] -= damage
+
+    # return the amount of damage taken
+    return damage, act
+
+
+# ### top-level game functions ###
+#
+
+def intro(fighters):
+    """Print the game header and announce the fighters"""
+    messenger.header("Welcome to the THUNDERDOME!")
+
+    # announce fighters
     print("\n  Tonight...\n")
     time.sleep(DELAY)
-    print(" ***", fighters[0]["name"], "the",
-          fighters[0]["species"].capitalize(),
-          "-vs-", fighters[1]["name"], "the",
-          fighters[1]["species"].capitalize(), "***\n\n")
-    response = input("ARE YOU READY TO RUMBLE?! ")
-    messenger.exit_if_quit(response)
+    messenger.center(
+        f"*** {fighters[0]['title']} -vs- {fighters[1]['title']} ***",
+        padding=2
+    )
 
+    # pause for input
+    messenger.ask("ARE YOU READY TO RUMBLE?!")
     messenger.line(".")
     print()
 
+
+def fight(fighters):
+    """Repeat rounds of the fight until one wins then return the winner"""
+
+    # ### set variables to initial values ###
+    #
+
+    # winning fighter
+    #
+    #   None is a special data type that means a variable is set, but it is
+    #   set to nothing
+    winner = None
+
+    # the index in the fighters list of the attacker in each round
+    current = 0
+
+    # ### rounds of the fight
+    #
     while winner is None:
+        # set the roles for this round
+        #
+        # `not <value>` is a handy way to switch 0 and 1
+        #   it is the same as `<value> == False`
         attacker = fighters[current]
         rival = fighters[not current]
 
-        response = input(f"\n{attacker['name']} FIGHT> ").lower()
-        messenger.exit_if_quit(response)
+        # pause for input
+        messenger.ask(f"\n{attacker['name']} FIGHT>")
 
-        damage = attack(rival)
-        act = random.choice(FIGHTIN_WORDS)
+        # the attack
+        damage, act = attack(rival)
 
+        # pause for effect, then print attack details
         time.sleep(DELAY)
         print()
         messenger.info(f"{attacker['name']} {act} {rival['name']}...\n")
 
+        # pause for effect, then print damage
         time.sleep(DELAY)
         messenger.center(f"-{damage} {rival['name']}")
+
+        # one more pause before the round ends
         time.sleep(DELAY)
 
-        if rival["health"] <= 0:
-            rival["health"] = 0
-            winner = not current
+        # check for a loser
+        if rival['health'] <= 0:
+            # don't let health drop below zero
+            rival['health'] = 0
+            # set the winner, this is now the last round
+            winner = attacker
 
+        # print updated fighter health
         print()
         for combatant in fighters:
             show(combatant)
 
+        # print a line at the end of every round
         messenger.line()
+
+        # flip current to the other fighter for the next round
         current = not current
 
-    # print the winnner
+    #
+    # ### end of fighting rounds
+
+    # return the winner
+    return winner
+
+
+def endgame(winner):
+    """Announce the winner"""
     print()
-    messenger.center(f"{fighters[winner]['name']} is Victorious!")
-    messenger.center(fighters[winner]["pic"])
+    messenger.center(f"{winner['name']} is Victorious!")
+    messenger.center(winner['pic'])
     messenger.line()
 
+
+def main():
+    """PyPet Fight Game"""
+
+    fighters = lotto()
+    init(fighters)
+
+    intro(fighters)
+    winner = fight(fighters)
+    endgame(winner)
+
+
+# ## Runner ##################################################################
 
 if __name__ == '__main__':
     main()
