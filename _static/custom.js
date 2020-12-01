e9160b0f138be02273b70e1af06b3aa6f4a54faa
Alissa Huskey <alissa.huskey@gmail.com>
2020-11-30 18:43:29 -0700

    .

--- _static/custom.js
+++ _static/custom.js
@@ -1,4 +1,19 @@
 // move the code-block captions to under the code blocks
+function addBodyId() {
+  url = window.location.href.split("#")[0]
+  pagename = url.slice(url.lastIndexOf("/")+1)
+
+  if (pagename.endsWith(".html")) {
+    pagename = pagename.slice(0, pagename.lastIndexOf(".html"))
+  }
+
+  console.log("addBodyId()> pagename: " + pagename)
+
+  node = document.getElementsByTagName("body")[0]
+  node.id = pagename
+}
+
+// move the code-block captions to under the code blocks
 function fixCaptions() {
   captions = $(".code-block-caption")
   for(i = 0; i < captions.length; i++) {
@@ -10,4 +25,5 @@ function fixCaptions() {
 }
 
 document.addEventListener("DOMContentLoaded", fixCaptions);
+document.addEventListener("DOMContentLoaded", addBodyId);
 
