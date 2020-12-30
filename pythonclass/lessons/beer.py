799309494568030ac6236d53a7d43fe70ef64be7
Alissa Huskey <alissa.huskey@gmail.com>
2020-12-29 19:14:42 -0700

    beer exercise: formatting

--- pythonclass/lessons/beer.py
+++ pythonclass/lessons/beer.py
@@ -9,7 +9,9 @@ Exercise from the Data Introspection lesson:
 
 import requests
 import colorful
+import textwrap
 from sys import stderr
+
 
 def abort(*messages):
     """Print message to stderr and exit"""
@@ -21,32 +23,42 @@ def main():
     """Fetch a random beer and print the name, description, and ingredients"""
     response = requests.get("http://api.punkapi.com/v2/beers/random")
     if not response.ok:
-        abort(
-            "Request failed:",
-            response.status_code,
-            response.reason)
+        abort("Request failed:", response.status_code, response.reason)
 
     data = response.json()
-
     beer = data[0]
 
     print()
-    print(beer['name'])
-    print("-" * len(beer['name']), "\n")
+    print(" ", beer['name'])
+    print(" ", "-" * len(beer['name']), "\n")
 
-    print(beer['description'], "\n")
+    lines = textwrap.wrap(
+        beer['description'],
+        initial_indent = "  ",
+        subsequent_indent="  ")
+
+    for line in lines:
+        print(line)
+
+    print()
 
     ingredients = set()
     for malt in beer['ingredients']['malt']:
-        ingredients.add(malt['name'] + " Malt")
+        name = malt['name']
+        if "malt" not in name.lower():
+            name += " Malt"
+        ingredients.add(name)
 
     for hop in beer['ingredients']['hops']:
-        ingredients.add(hop['name'] + " Hops")
+        name = hop['name']
+        if "hops" not in name.lower():
+            name += " Hops"
+        ingredients.add(name)
 
     ingredients.add(beer['ingredients']['yeast'])
 
     for item in sorted(ingredients):
-        print(f"  * {item}")
+        print(f"    * {item}")
 
     print()
 
