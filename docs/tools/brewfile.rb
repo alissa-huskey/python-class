6ea557b4eafa2ff990a34643e93734c66b5b373e
Alissa Huskey <alissa.huskey@gmail.com>
2022-01-29 01:08:58 -0700

    add homebrew guide

---
+++ docs/tools/brewfile.rb
@@ -0,0 +1,21 @@
+# minimum package list for python-class
+#
+# updates can be found at:
+# https://github.com/alissa-huskey/python-class/blob/master/docs/tools/brewfile.rb
+# #
+
+cask "visual-studio-code"                 # editor
+brew "git"                                # most recent git version
+cask "discord"                            # Discord app
+brew "mas"                                # app store CLI tool
+brew "pyenv"                              # install and manage Python versions
+
+# python dependencies
+# -------------------
+
+brew "zlib"
+brew "openssl"
+brew "readline"
+brew "xz"
+brew "bzip2"
+
